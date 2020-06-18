147199e9fd0a6b84d3f1a2ea89b4904e7f65aad7
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-06-18 13:25:56 +0300

    add form_for and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :edit
+    end
   end
 
   def create
