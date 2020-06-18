20bf20fff73da8e4386dfafd310c2afbbd51dc6f
Christopher Amanor <kriskit7@gmail.com>
2020-06-18 10:46:44 +0000

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def edit
     @user = User.find(params[:id])
   end
+
   def update
     @user = User.find(params[:id])
     @user.update_attributes(user_params)
@@ -25,7 +29,7 @@ class UsersController < ApplicationController
     end
   end
 
-   private
+  private
 
   def user_params
     params.require(:user).permit(:email, :password, :username)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  validates :username, presence: true, length: {minimum:3, maximum:20}
-  validates :email, presence: true, length: {minimum:3, maximum:20},format: {with: URI::MailTo::EMAIL_REGEXP}
-  validates :password, presence: true, length: {minimum:3, maximum:20}
+  validates :username, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :email, presence: true, length: { minimum: 3, maximum: 20 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 3, maximum: 20 }
 end
