37346d769d0d76738e361230f685be40d307e6df
Christopher Amanor <kriskit7@gmail.com>
2020-06-18 08:56:39 +0000

    Add create method to the users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: {minimum:3, maximum:12}
-  validates :email, presence: true, length: {minimum:3, maximum:12},format: {With: URI::MailTo::EMAIL_REGEXP}
-  validates :password, presence: true, length: {minimum:3, maximum:12}
+  validates :username, presence: true, length: {minimum:3, maximum:20}
+  validates :email, presence: true, length: {minimum:3, maximum:20},format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :password, presence: true, length: {minimum:3, maximum:20}
 end
